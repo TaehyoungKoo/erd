b6f5cb34acc6a55dc4d87c4bd05e8a075a193b36
unknown <Angelo@DESKTOP-1PS1HSS.lgcns.com>
2020-01-20 14:11:51 +0900

    Adds single page sample

---
+++ src/components/SinglePageSample.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class='page'>
+    <div class='page-title'>
+      Single Detail
+    </div>
+    <div class='page-body'>
+      <div class='field-group'>
+        <div class='field-label'>
+          <label>Name</label>
+        </div>
+        <div class='field-controls'>
+          <input v-model='name' type='input'>
+        </div>
+      </div>
+      <div class='field-group'>
+        <div class='field-label'>
+          <label>Description</label>
+        </div>
+        <div class='field-controls'>
+          <textarea v-model='description'></textarea>
+        </div>
+      </div>
+      <div class='button-group'>
+        <button @click='saveEntity'>Save</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      name: '',
+      description: '',
+    }
+  },
+
+  methods: {
+    saveEntity: function () {
+      const entity = {
+        name: this.name,
+        description: this.description,
+      }
+
+      this.$store.state.entity.push(entity)
+    },
+  },
+}
+</script>
