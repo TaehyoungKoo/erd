53559455ba347b89dbacbe8da9bf1e987a4f0d91
Angelo <kooangelo@gmail.com>
2020-01-21 14:40:00 +0900

    Adds select/unSelect radioButton and check/unCheck checkbox functions

--- tests/unit/elements.js
+++ tests/unit/elements.js
@@ -13,18 +13,30 @@ function findLabel(wrapper, text) {
     console.log(`There is no [${text}] label`)
 }
 
-
+//------------------------------------------------------------------
 function findRadioButtonByValue(wrapper, value) {
   const radioList = wrapper.findAll('input[type="radio"]')
 
   for (var i = 0; i < radioList.length; i++) {
 
-    //console.log(radioList.at(i).attributes().value)
-
     if (radioList.at(i).attributes().value === value) {
       return radioList.at(i)
     }
   }
+}
+
+function selectRadioButton(wrapper, text) {
+  const radioButton = findRadioButton(wrapper, text)
+
+  radioButton.element.checked = 'checked'
+  radioButton.trigger('change')
+}
+
+function unSelectRadioButton(wrapper, text) {
+  const radioButton = findRadioButton(wrapper, text)
+
+  radioButton.element.checked = ''
+  radioButton.trigger('change')
 }
 
 function findRadioButton(wrapper, text) {
@@ -41,7 +53,7 @@ function findRadioButton(wrapper, text) {
   }  
 }
 
-
+//------------------------------------------------------------------
 function findCheckboxByValue(wrapper, value) {
   const checkboxList = wrapper.findAll('input[type = "checkbox"]')
 
@@ -53,6 +65,7 @@ function findCheckboxByValue(wrapper, value) {
 }
 
 function findCheckbox(wrapper, text) {
+  let selectedCheckbox
   const checkboxList = wrapper.findAll('div[id^="checkbox-container"]')
 
   for (var i = 0; i < checkboxList.length; i++) {
@@ -60,17 +73,79 @@ function findCheckbox(wrapper, text) {
     const label = checkboxList.at(i).find('label')
 
     if (label.text() === text) {
-      return checkbox
+      selectedCheckbox = checkbox
     }
-  }  
+  }
+
+  return {
+    object: selectedCheckbox,
+    exist: (selectedCheckbox === undefined ? false : true),
+    
+    checked: (
+      selectedCheckbox !== undefined && 
+        selectedCheckbox.element.checked ? true : false
+        //or selectedCheckbox.attributes().checked === 'checked' ? true : false
+    ),
+
+    disabled: (
+      selectedCheckbox !== undefined &&
+      selectedCheckbox.element.disabled ? true : false
+    ),
+  }
 }
+
+function checkCheckbox(wrapper, text) {
+  const checkbox = findCheckbox(wrapper, text).object
+
+  checkbox.element.checked = 'checked'
+  checkbox.trigger('change')
+}
+
+function unCheckCheckbox(wrapper, text) {
+  const checkbox = findCheckbox(wrapper, text).object
+
+  checkbox.element.checked = ''
+  checkbox.trigger('change')
+}
+
+//------------------------------------------------------------------
+function findInputById(wrapper, id) {
+  const inputList = wrapper.findAll('input[type = "input"]')
+
+  for (var i = 0; i < inputList.length; i++) {
+    if (inputList.at(i).attributes().id === id) {
+      return inputList.at(i)
+    }
+  }
+}
+
+//------------------------------------------------------------------
+function findTextareaById(wrapper, id) {
+  const textareaList = wrapper.findAll('textarea')
+
+  for (var i = 0; i < textareaList.length; i++) {
+    if (textareaList.at(i).attributes().id === id) {
+      return textareaList.at(i)
+    }
+  }
+}
+
+
 
 export {
   findLabel, 
   
   findRadioButton, 
   findRadioButtonByValue,
+  selectRadioButton,
+  unSelectRadioButton,
   
   findCheckbox,
   findCheckboxByValue,
+  checkCheckbox,
+  unCheckCheckbox,
+
+  findInputById,
+
+  findTextareaById,
 }
